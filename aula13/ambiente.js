cd9ce8cfafc02315bc1a8ad6ba7bcccb16b5700a
Matheus Henrique <131389471+CloneTz@users.noreply.github.com>
2023-10-04 11:03:28 -0300

    aula13 finalizada

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+let c = 1 
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/*
+
+let c = 1 
+while (c <= 10) {
+    console.log(`passo ${c}`)
+    c++
+}
+ 
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
