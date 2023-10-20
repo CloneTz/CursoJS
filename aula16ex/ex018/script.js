2706331508afc800c61ad269ccee04e74a30ae97
Matheus Henrique <131389471+CloneTz@users.noreply.github.com>
2023-10-20 09:40:28 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,24 +1,28 @@
 let num = document.querySelector("input#fnum");
 let lista = document.querySelector("select#flista");
 let res = document.querySelector("div#res");
-let valores = []
+let valores = [];
 
 function isNumero(n) {
-    if(Number(n) >= 1 && Number(n) <= 100) {
-        return true
-    } else {
-        return false
-    }
+  if (Number(n) >= 1 && Number(n) <= 100) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function inLista(n, l) {
-
+  if (l.indexOf(Number(n)) != -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value)) {
-
-    } else {
-        alert('Vlaor inválido ou já encontrado na lista.')
-    }
+  if (isNumero(num.value) && !inLista(num.value, valores)) {
+    window.alert("Tudo OK!");
+  } else {
+    alert("Valor inválido ou já encontrado na lista.");
+  }
 }
